6817838a8aa44213612b20bbd7e96e4e0da641b3
Adele Richard <adelerichard@Adeles-Mac-mini.local>
2025-07-30 15:29:06 +0200

    Move Go files to root and simplify build for Render
    - Move main.go, database.go, models.go from cmd/server/ to root directory
    - Update render.yaml buildCommand to 'go build -o bin/server .'
    - This ensures all Go files are properly included in Render's build process
    - Simpler project structure that's more compatible with Render's Go detection

---
+++ auth-api/database.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	var err error
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"))
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	log.Println("Database connection established")
+
+	// Auto-migrate the schema
+	DB.AutoMigrate(&SessionDB{}, &PlayerDB{}, &StoryDB{}, &ChatMessageDB{}, &VoteDB{}, &JiraConnectionDB{})
+	log.Println("Database schema migrated")
+}
+
